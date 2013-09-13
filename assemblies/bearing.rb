de38b942ad424a9ffd9a3a08f942cb6a6da3788b
Joachim Glauche <webmaster@joaz.de>
2013-09-13 19:20:17 +0200

    belt holder wip

--- assemblies/bearing.rb
+++ assemblies/bearing.rb
@@ -1,5 +1,5 @@
 # this should go to crystalscad standard library
-class Bearing
+class Bearing < CrystalScad::Assembly
   attr_accessor :size
   def initialize(args={})		
     @args = args
@@ -7,7 +7,8 @@ class Bearing
     @args[:sealing] ||= "ZZ"
     @args[:margin] ||= 0.0
     @args[:outer_rim_cut] ||= 0.0
-    
+    @args[:flange] ||= false
+
     
     prepare_data
       
@@ -16,7 +17,7 @@ class Bearing
   
   def prepare_data
     chart = {"608" => {inner_diameter:8,outer_diameter:22,thickness:7},
-             "624" => {inner_diameter:4,outer_diameter:13,thickness:5},
+             "624" => {inner_diameter:4,outer_diameter:13,thickness:5, inner_rim:6.3, outer_rim:11.0, flange_diameter:15, flange_width:1},
              "625" => {inner_diameter:5,outer_diameter:16,thickness:5, inner_rim:7.5, outer_rim:13.5},
     
     
@@ -29,9 +30,12 @@ class Bearing
   end
   
   def output
-     bearing = cylinder(d:@size[:outer_diameter]+@args[:margin],h:@size[:thickness])
-     bearing+= cylinder(d:@size[:outer_rim],h:@args[:outer_rim_cut]).translate(z:-@args[:outer_rim_cut]/2) 
-     bearing
+    bearing = cylinder(d:@size[:outer_diameter]+@args[:margin],h:@size[:thickness])
+    bearing+= cylinder(d:@size[:outer_rim],h:@args[:outer_rim_cut]).translate(z:-@args[:outer_rim_cut]/2) 
+    if @args[:flange] == true
+      bearing += cylinder(d:@size[:flange_diameter].to_f, h:@size[:flange_width])      
+    end
+    bearing
   end
   
   def show
@@ -51,6 +55,12 @@ class Bearing
       bearing-= cylinder(d:@size[:inner_diameter],h:@size[:thickness]+0.2).translate(z:-0.1)    
       bearing.color("LavenderBlush")
     end  
+    if @args[:flange] == true
+      flange = cylinder(d:@size[:flange_diameter].to_f, h:@size[:flange_width].to_f)
+      flange-= cylinder(d:@size[:outer_diameter],h:@size[:thickness]+0.2).translate(z:-0.1)
+      bearing+=flange.color("Silver")
+    end
+    
     bearing
   end
     	  
--- bulldozer.rb
+++ bulldozer.rb
@@ -45,24 +45,35 @@ assembly+= YPlateAssembly.new(length:470,rod_size:12,position:100).show.translat
 #assembly+=Lm_uu.new(inner_diameter:12).show.rotate(x:90).translate(x:15,z:20,y:200)
 #assembly+=Lm_uu.new(inner_diameter:12).show.rotate(x:90).translate(x:210,z:20,y:150)
  
-assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2), TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30))    
-#assembly+=Bolt.new(3,35).output
-#assembly=TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8).show
+#assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2), TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30))    
 
-#sketch
-# y motor sketch
+class YBeltHolder < CrystalScad::Assembly
+  
+  def show
+    base = cube([40,10,16])
+    
+    base += cube([35,10,10]).translate(z:16)
+    base -= cube([20.2,10.2,1.5]).translate(x:15-0.1,y:-0.1,z:16)
+    
+    
+    base.mirror(z:1)
+  end
+
+end
+
 assembly += Nema17.new.show.rotate(y:90).translate(x:95,y:460,z:-7-14)
 assembly += Belt.new.show.translate(x:150,y:40,z:-14)
+assembly += YBeltHolder.new().show.translate(x:135,y:230,z:3)
 
 
-assembly+= ZAxisAssembly.new.show 
+#assembly+= ZAxisAssembly.new.show 
+
+#assembly += Bearing.new(:type => "624", :flange=>true).show.rotate(y:90)
 
 
-
-
-
-
-#puts @@bom.output
-puts "$fn=64;"+assembly.output
+file = File.open("bom.txt","w")
+file.puts @@bom.output
+file.close
+puts "$fn=64;"+assembly.scad_output
 #puts TSlot.new(size:40,configuration:2).output
 
