e5709c26c9418de7c83ad3cd3f8a3f032fea8ef5
Joachim Glauche <webmaster@joaz.de>
2013-09-17 17:53:20 +0200

    more wip

--- bulldozer.rb
+++ bulldozer.rb
@@ -38,17 +38,17 @@ assembly=CrystalScadObject.new
 
 
 
-assembly+= YPlateAssembly.new(length:470,rod_size:12,position:100).show.translate(z:3.5,y:30,x:35)
+#assembly+= YPlateAssembly.new(length:470,rod_size:12,position:100).show.translate(z:3.5,y:30,x:35)
 
 assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2), TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30))    
 
 
 
 
-assembly += Nema17.new.show.rotate(y:90).translate(x:95,y:460,z:-7-14)
-assembly += Belt.new.show.translate(x:150,y:40,z:-14)
-assembly += YBeltHolder.new.show.translate(x:135,y:230,z:3)
-assembly += YBeltIdler.new.show.rotate(y:90).translate(x:123,y:40,z:-8)
+#assembly += Nema17.new.show.rotate(y:90).translate(x:95,y:460,z:-7-14)
+#assembly += Belt.new.show.translate(x:150,y:40,z:-14)
+#assembly += YBeltHolder.new.show.translate(x:135,y:230,z:3)
+#assembly += YBeltIdler.new.show.rotate(y:90).translate(x:123,y:40,z:-8)
 
 # FIXME: YBeltHolder and BedPlateBearingMount are both missing wood screws 
 
@@ -62,29 +62,42 @@ class XAxisAssembly < CrystalScad::Assembly
   end
   
   def show
-    axis = TSlot.new(size:30).show(300).rotate(y:90).color("Silver")
+    # z bearings
+    axis  = Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:40,x:15,z:-50)
+    axis += Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:40,x:15,z:35)
+    
+    axis += Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:40,x:15+270,z:-30)
+    axis += Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:40,x:15+270,z:35)
+
+    axis += TSlot.new(size:30).show(300).rotate(y:90).color("Silver")
     axis += Rod.new(length:300).show.rotate(z:-90).translate(y:15,z:15)
     axis += Rod.new(length:300).show.rotate(z:-90).translate(y:-10,z:-15)
     axis += Lm_uu.new(inner_diameter:12).rotate(y:90).translate(y:15,z:15,x:@args[:position]+20)
     axis += Lm_uu.new(inner_diameter:12).rotate(y:90).translate(y:-10,z:-15,x:@args[:position])
     axis += Lm_uu.new(inner_diameter:12).rotate(y:90).translate(y:-10,z:-15,x:@args[:position]+40)
-    axis += Belt.new(longest_side_length:280,top_side_length:250).show.rotate(z:-90,y:90).translate(x:13,y:-10,z:10)
-    axis += Nema17.new.show.rotate(x:180).translate(x:13,y:-5,z:75)
+    #axis += Belt.new(longest_side_length:280,top_side_length:250).show.rotate(z:-90,y:90).translate(x:13,y:-10,z:10)
+    #axis += Nema17.new.show.rotate(x:180).translate(x:13,y:-5,z:75)
     
-  #  axis += Nema17.new.show.rotate(x:90).translate(y:45,z:-5,x:-60)
-  end
+    axis +=  Nema17.new.show.rotate(x:90).translate(x:30,y:95,z:25)
+    axis += Belt.new(longest_side_length:270,top_side_length:250,position:20).show.rotate(z:-90,y:180).translate(x:30,y:30,z:25-7)
+   
+    # now need parts that fit into the t-slot on the ends and get tightened by M8
+    # t-slot dimensions
+    # opening = 8.1
+    # wall 2.3
+    # rectangular slot beind wall: 16.2x2.1-2.2
+   
+   end
 end
 
 assembly += Rod.new(length:370).show.rotate(x:90).translate(y:280,x:15,z:0)
 assembly += Rod.new(length:370).show.rotate(x:90).translate(y:280,x:15+270,z:0)
-assembly += Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:280,x:15,z:70)
-assembly += Lm_uu.new(inner_diameter:12).rotate(x:0).translate(y:280,x:15+270,z:70)
 
 
 #x_sketch += Bolt.new(4,20).rotate(x:90).translate(y:20,z:-35,x:45)
 #x_sketch += Nut.new(4).rotate(x:90).translate(y:0,z:-35,x:45)
 
-assembly += XAxisAssembly.new.show.translate(z:100+0,y:240)
+assembly += XAxisAssembly.new.show.translate(z:90+0,y:240)
 
 
 file = File.open("bom.txt","w")
