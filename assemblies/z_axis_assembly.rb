6f0d220390b26e4b8e57eaff02bef258470b4045
Joachim Glauche <webmaster@joaz.de>
2013-09-18 17:20:17 +0200

    more wip

--- assemblies/z_axis_assembly.rb
+++ assemblies/z_axis_assembly.rb
@@ -5,13 +5,13 @@ class ZAxisAssembly
     @args[:left_pos] = -30  # left of y t-slot
     @args[:right_pos] = 325 # right of y-tslot
     @args[:height] = 430 # total height from bottom
-    
+    @args[:tslot_simple] ||= false
   end
  
   def show
-      tslot_left = TSlotMachining.new(size:30,configuration:2,holes:"front",bolt_size:5,bolt_length:30)
-      tslot_right = TSlotMachining.new(size:30,configuration:2,holes:"front",bolt_size:5,bolt_length:30)
-      tslot_top = TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30)
+      tslot_left = TSlotMachining.new(size:30,configuration:2,holes:"front",bolt_size:5,bolt_length:30,simple:@args[:tslot_simple])
+      tslot_right = TSlotMachining.new(size:30,configuration:2,holes:"front",bolt_size:5,bolt_length:30,simple:@args[:tslot_simple])
+      tslot_top = TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30,simple:@args[:tslot_simple])
   
       assembly = tslot_left.show(@args[:height]).translate(x:@args[:left_pos],y:@args[:position],z:-60).color("Silver")
       assembly += tslot_right.show(@args[:height]).mirror(x:1).translate(x:@args[:right_pos],y:@args[:position],z:-60).color("Silver")
--- bulldozer.rb
+++ bulldozer.rb
@@ -14,8 +14,8 @@ include CrystalScad
 
 
 @@printed_color="Bisque"
-
-
+@tslot_simple = true
+ 
 
 def tslot_rectangle(x,y,tslot_type_x,tslot_type_y)
 	size = tslot_type_x.args[:size]
@@ -40,7 +40,7 @@ assembly=CrystalScadObject.new
 
 #assembly+= YPlateAssembly.new(length:470,rod_size:12,position:100).show.translate(z:3.5,y:30,x:35)
 
-assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2), TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30))    
+assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2,simple:@tslot_simple), TSlotMachining.new(size:30,configuration:2,holes:"front,back",bolt_size:8,bolt_length:30,simple:@tslot_simple))    
 
 
 
@@ -53,7 +53,41 @@ assembly+=tslot_rectangle(225+60+10,520, TSlot.new(size:30,configuration:2), TSl
 # FIXME: YBeltHolder and BedPlateBearingMount are both missing wood screws 
 
 
-assembly+= ZAxisAssembly.new.show 
+assembly+= ZAxisAssembly.new(tslot_simple:true).show
+
+class AcmeNut < CrystalScad::Assembly
+  def initialize(args={})
+    @args = args
+    @args[:shape] = "cylinder"
+    super
+  end
+  
+  def show
+    return cylinder_nut(false) if @args[:shape] == "cylinder"
+    return hex_nut(false) if @args[:shape] == "hexagonal"
+  end
+
+  def output
+    return cylinder_nut(true) if @args[:shape] == "cylinder"
+    return hex_nut(true) if @args[:shape] == "hexagonal"
+  end
+  
+  def cylinder_nut(output)
+    nut = cylinder(d:22,h:20)
+    if output == false
+      nut -= cylinder(d:10,h:20.2).translate(z:-0.1)
+    else
+      nut += cylinder(d:12,h:30).translate(z:-5)
+    end
+    nut.color("Goldenrod")
+  end
+  
+  def hex_nut
+    # todo
+  end
+  
+  
+end
 
 class XAxisAssembly < CrystalScad::Assembly
   def initialize(args={})
@@ -77,6 +111,7 @@ class XAxisAssembly < CrystalScad::Assembly
     axis += Lm_uu.new(inner_diameter:12).rotate(y:90).translate(y:-10,z:-15,x:@args[:position]+40)
     #axis += Belt.new(longest_side_length:280,top_side_length:250).show.rotate(z:-90,y:90).translate(x:13,y:-10,z:10)
     #axis += Nema17.new.show.rotate(x:180).translate(x:13,y:-5,z:75)
+    axis += AcmeNut.new.show.translate(x:-23,y:15,z:-30)
     
     axis +=  Nema17.new.show.rotate(x:90).translate(x:30,y:95,z:25)
     axis += Belt.new(longest_side_length:270,top_side_length:250,position:20).show.rotate(z:-90,y:180).translate(x:30,y:30,z:25-7)
