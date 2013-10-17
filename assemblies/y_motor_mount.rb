1fe5ddde357a394307f2e8ff6a23c983eda11ffd
Joachim Glauche <webmaster@joaz.de>
2013-10-17 16:25:14 +0200

    trying to make y motor mount less prone for breaking

--- assemblies/y_motor_mount.rb
+++ assemblies/y_motor_mount.rb
@@ -12,10 +12,10 @@ class YMotorMount < CrystalScad::Assembly
   def part(show)
     motor_mount = MotorMount.new
     res += motor_mount.part(show).translate(x:-12)
-
    
     res += slot_mount(show).rotate(x:-90).translate(y:-27,z:50)
     res += slot_mount(show).rotate(x:-90).translate(x:-30,y:-27,z:50)
+    res -= cylinder(d:3,h:100).rotate(y:90).translate(x:-40,y:-20,z:7)
          
     res
   end
@@ -25,7 +25,7 @@ class YMotorMount < CrystalScad::Assembly
   	#.translate(x:-20,y:-25) 
     
     
-		mount -= long_slot(d:4.4,h:10,l:30).rotate(z:90).translate(x:15,y:10,z:0)
+		mount -= long_slot(d:4.4,h:10,l:30).rotate(z:90).translate(x:15,y:8,z:0)
 		bolt = Bolt.new(4,12)
 		washer = Washer.new(4.3)		
 		mount += bolt.show.mirror(z:1).translate(x:15,y:15,z:7) if show
