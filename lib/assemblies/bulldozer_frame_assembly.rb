9ff2b379c7d28164e73d56d3c1eef77973ea4fc3
Joachim Glauche <webmaster@joaz.de>
2013-12-16 13:55:30 +0100

    rubber dampeners there are a bad idea

--- lib/assemblies/bulldozer_frame_assembly.rb
+++ lib/assemblies/bulldozer_frame_assembly.rb
@@ -24,7 +24,7 @@ class BulldozerFrameAssembly < CrystalScad::Assembly
 
 	def part
 		# lower base tslot
-		res  = tslot_rectangle_with_dampeners(@tslot_x,@tslot_y, @tslot_double,@tslot_double) 
+		res  = tslot_rectangle(@tslot_x,@tslot_y, @tslot_double,@tslot_double) 
 		# upper tslot
 		res += tslot_rectangle(@tslot_x,@tslot_y, @tslot_single,@tslot_single).translate(z:@tslot_z) 
 	
@@ -68,31 +68,6 @@ class BulldozerFrameAssembly < CrystalScad::Assembly
 		r = r.color("Silver")
 	end
 
-	def tslot_rectangle_with_dampeners(x,y,tslot_type_x,tslot_type_y)
-		size = tslot_type_x.args[:size]
-		# x 
-		r = tslot_type_x.show(x-size*2-30).rotate(x:-90,z:-90).translate(x:size+15,y:size)
-		r += tslot_type_x.show(x-size*2-30).rotate(x:-90,z:-90).translate(x:size+15,y:y)   
-
-		# y
-		r += tslot_type_y.show(y).rotate(x:-90)
-		r += tslot_type_y.show(y).rotate(x:-90).mirror(x:1).translate(x:x)
-		
-		r = r.color("Silver")
-		# add the dampeners
-		r += RubberDampener.new.show.rotate(y:90).translate(x:30,y:15,z:-15)	 
-		r += RubberDampener.new.show.rotate(y:90).translate(x:30,y:15,z:-15-30)	 
-		r += RubberDampener.new.show.rotate(y:90).translate(x:30,y:y-15,z:-15)	 
-		r += RubberDampener.new.show.rotate(y:90).translate(x:30,y:y-15,z:-15-30)	 
-
-		r += RubberDampener.new.show.rotate(y:-90).translate(x:x-30,y:15,z:-15)	 
-		r += RubberDampener.new.show.rotate(y:-90).translate(x:x-	30,y:15,z:-15-30)	 
-		r += RubberDampener.new.show.rotate(y:-90).translate(x:x-30,y:y-15,z:-15)	 
-		r += RubberDampener.new.show.rotate(y:-90).translate(x:x-	30,y:y-15,z:-15-30)	 
-
-
-		return r
-	end
 
 
 end
