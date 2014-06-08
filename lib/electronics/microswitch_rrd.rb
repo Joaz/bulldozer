5b6d2fadcb521e4e20a7f90c3be866011962a42a
Joachim Glauche <info@gli-concept.de>
2014-06-08 09:08:33 +0200

    add button sketch to visualize its switching position

--- lib/electronics/microswitch_rrd.rb
+++ lib/electronics/microswitch_rrd.rb
@@ -4,6 +4,8 @@ class MicroswitchRrd < CrystalScad::Assembly
     res = cube([40,16,1.6]).color("white")
     # switch
     res += cube([13,6.5,6]).translate(x:20,y:10,z:1.6).color("black")
+    res += cube([1.5,1.5,3]).translate(x:22,y:16,z:3.2).color("blue")
+
     
     res -= cylinder(d:3.5,h:2).translate(x:17,y:13.5,z:-0.1)
     res -= cylinder(d:3.5,h:2).translate(x:17+19,y:13.5,z:-0.1)
